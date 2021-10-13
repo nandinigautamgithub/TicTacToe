e133257cfa01656c4e62a2606d72abe6fd826c66
nandinigautam <nandinigautam28@gmail.com>
2021-10-13 15:05:02 +0530

    TicTacToe

---
+++ Tictactoe.java
@@ -0,0 +1,19 @@
+package com.board;
+import java.util.*;
+
+
+public class Tictactoe {
+	
+	public Tictactoe() {
+	Tictactoe tictactoe = new Tictactoe();
+
+	}
+	
+	public static void actionPerformed() {
+		// TODO Auto-generated method stub
+		
+	System.out.println("Welcome to 3x3 Tic Tac Toe.");
+	}
+
+}
+
